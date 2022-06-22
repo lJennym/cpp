34a56c7e6241ae258fbb222bc1024af355d6878b
Jenny <98678108+lJennym@users.noreply.github.com>
2022-06-21 17:34:10 -1200

    Add files via upload

---
+++ LP 2/Proyecto 1/Menu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "starShip.h"
+#include "snake.h"
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	starShip();
+	//snake();
+	
+	return 222;
+}
